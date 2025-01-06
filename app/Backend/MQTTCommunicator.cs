2c7c2b37664767c0c5d06cb6b09d7c02180aa919
witrellesp <william.trelles1@eduvaud.ch>
2025-01-06 15:06:01 +0100

    code (mediaplayer) : changement de topic
    [1m][DONE]
    media/player au lieu de powercher

--- app/Backend/MQTTCommunicator.cs
+++ app/Backend/MQTTCommunicator.cs
@@ -11,7 +11,7 @@ namespace Backend;
 
 public class MqttCommunicator : ICommunicator
 {
-    private const string DefaultTopic = "powercher";
+    private const string DefaultTopic = "media/player";
     private readonly string _brokerIp;
     private IMqttClient _mqttClient;
     private readonly ILogger _logger;
